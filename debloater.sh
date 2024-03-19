07ae56d061806c38c67971b504c905e4fb8bc043
thatonebyte <thatonebyte@proton.me>
2024-03-18 23:28:01 -0400

    Better

--- debloater.sh
+++ debloater.sh
@@ -1,103 +1,109 @@
 #!/bin/bash
 
 echo
-echo https://github.com/thatonemegabyte/debiandebloat
+echo "GitHub Repository: https://github.com/thatonemegabyte/debiandebloat"
 echo
-echo "Thank you for using my script ^w^"
-echo Now lets get to removing some packages!
+echo "Thank you for using my script! (^w^)"
+echo "Now let's get to removing some packages!"
 echo ""
-echo note: this may remove packages you still want!
-echo ========================
+echo "Note: This may remove packages you still want!"
+echo "==============================================="
 echo ""
-echo -n "Continue? [y/n] "
+read -p "Continue? [y/n]: " input
 
-read input
-
-if [[ $input == "y" || $input == "Y" ]]; then
+if [[ $input =~ ^[Yy]$ ]]; then
     echo ""
     echo "Continuing..."
-    sudo apt-get remove deja-dup -y
-	sudo apt-get remove goldendict -y
-	sudo apt-get remove gnote -y
-	sudo apt-get remove yelp -y
-	sudo apt-get remove totem -y
-	sudo apt-get remove brasero -y
-	sudo apt-get remove brasero-common -y
-	sudo apt-get remove cheese -y
-	sudo apt-get remove sound-juicer -y
-	sudo apt-get remove gnome-sound-recorder -y
-	sudo apt-get remove gnome-robots -y
-	sudo apt-get remove gnome-chess -y
-	sudo apt-get remove aisleriot -y
-	sudo apt-get remove five-or-more -y
-	sudo apt-get remove gnome-mahjongg -y
-	sudo apt-get remove tali -y
-	sudo apt-get remove four-in-a-row -y
-	sudo apt-get remove gnome-klotski -y
-	sudo apt-get remove gnome-mines -y
-	sudo apt-get remove gnome-nibbles -y
-	sudo apt-get remove gnome-2048 -y
-	sudo apt-get remove gnome-sudoku -y
-	sudo apt-get remove gnome-taquin -y
-	sudo apt-get remove gnome-tetravex -y
-	sudo apt-get remove hitori -y
-	sudo apt-get remove lightsoff -y
-	sudo apt-get remove quadrapassel -y
-	sudo apt-get remove swell-foop -y
-	sudo apt-get remove iagno -y
-	sudo apt-get remove eog -y
-	sudo apt-get remove simple-scan -y
-	sudo apt-get remove shotwell -y
-	sudo apt-get remove pidgin -y
-	sudo apt-get remove hexchat -y
-	sudo apt-get remove transmission-gtk -y
-	sudo apt-get remove remmina -y
-	sudo apt-get remove libreoffice* -y
-	sudo apt-get remove anthy -y
-	sudo apt-get remove kasumi -y
-	sudo apt-get remove uim -y
-	sudo apt-get remove uim-data -y
-	sudo apt-get remove uim-mozc -y
-	sudo apt-get remove uim-xim -y
-	sudo apt-get remove im-config -y
-	sudo apt-get remove mozc-data -y
-	sudo apt-get remove mozc-utils-gui -y
-	sudo apt-get remove fcitx5-data -y
-	sudo apt-get remove fcitx-bin -y
-	sudo apt-get remove fcitx-module-dbus -y
-	sudo apt-get remove fcitx-config-common -y
-	sudo apt-get remove hunspell -y
-	sudo apt-get remove aspell -y
-	sudo apt-get remove eject -y
-	sudo apt-get remove vino -y
-	sudo apt-get remove gnome-software -y
-	sudo apt-get remove xterm -y
-	sudo apt-get remove mlterm-common -y
-	sudo apt-get remove mlterm -y
-	sudo apt-get remove mlterm-tiny -y
-	sudo apt-get remove xiterm+thai -y
-	sudo apt-get remove malcontent -y
-	sudo apt-get remove gnome-maps -y
-	sudo apt-get remove gnome-color-manager -y
-	sudo apt-get remove gnome-clocks -y
-	sudo apt-get remove calander -y
-	sudo apt-get remove file-roller -y
-	sudo apt-get remove gnome-music -y
-	sudo apt-get remove gnome-weather -y
-	sudo apt-get remove rhythmbox* -y
-	sudo apt-get remove evolution -y
-	sudo apt-get remove gnome-photos -y
-	sudo apt-get remove baobab -y
-	sudo apt-get remove gnome-disks
-	sudo apt-get remove gnome-logs -y
-	sudo apt-get remove nm-connection-editor -y
-	sudo apt-get remove gnome-extensions -y
-	sudo apt autoremove
-	echo All Done!
+    
+    # List of packages to be removed
+    packages=(
+        deja-dup
+        goldendict
+        gnote
+        yelp
+        totem
+        brasero
+        brasero-common
+        cheese
+        sound-juicer
+        gnome-sound-recorder
+        gnome-robots
+        gnome-chess
+        aisleriot
+        five-or-more
+        gnome-mahjongg
+        tali
+        four-in-a-row
+        gnome-klotski
+        gnome-mines
+        gnome-nibbles
+        gnome-2048
+        gnome-sudoku
+        gnome-taquin
+        gnome-tetravex
+        hitori
+        lightsoff
+        quadrapassel
+        swell-foop
+        iagno
+        eog
+        simple-scan
+        shotwell
+        pidgin
+        hexchat
+        transmission-gtk
+        remmina
+        libreoffice*
+        anthy
+        kasumi
+        uim
+        uim-data
+        uim-mozc
+        uim-xim
+        im-config
+        mozc-data
+        mozc-utils-gui
+        fcitx5-data
+        fcitx-bin
+        fcitx-module-dbus
+        fcitx-config-common
+        hunspell
+        aspell
+        eject
+        vino
+        gnome-software
+        xterm
+        mlterm-common
+        mlterm
+        mlterm-tiny
+        xiterm+thai
+        malcontent
+        gnome-maps
+        gnome-color-manager
+        gnome-clocks
+        calendar
+        file-roller
+        gnome-music
+        gnome-weather
+        rhythmbox*
+        evolution
+        gnome-photos
+        baobab
+        gnome-disks
+        gnome-logs
+        nm-connection-editor
+        gnome-extensions
+    )
+
+    # Loop through the packages and remove them
+    for package in "${packages[@]}"; do
+        sudo apt-get remove "$package" -y
+    done
+
+    sudo apt autoremove
+    echo "All Done!"
 else
     echo ""
     echo "Exiting..."
 fi
-
-
 
